63b9d0ed6790813108ff71e7ccd39ad8093ae5a9
augustinmuster <augustin@must-r.com>
2023-08-24 13:19:39 +0200

    update

--- src/HelloWorld.jl
+++ src/HelloWorld.jl
@@ -1,15 +1,25 @@
 module HelloWorld
 
-export test1, TestMod
+export test1, TestMod, TestMod3
 
 function test1()
 	return 1
 end
 
+
+include("test.jl")
 module TestMod
 	export test2
 	function test2()
 		return 2
+	end	
+end
+
+module TestMod2
+	export test3
+	using .TestMod
+	function test3()
+		return test1()+TestMod.test2()
 	end	
 end
 end
---
+++ src/test.jl
@@ -0,0 +1,6 @@
+module TestMod3
+export test4
+function test4()
+return 4
+end
+end
